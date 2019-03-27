e009b03bf325861d9254586fc60ee238e8099947
Anja <rasoloanja@gmail.com>
2019-03-27 14:46:51 -0400

    project init

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients) //this return an array of the ingredients but not with the number
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+              return  <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
+        
+    return(
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+  /*  constructor(props) {
+        constructor(props) {
+            super(props);
+            this.state = {...}
+        }
+    }
+    */
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render() { //the most important one - we always need to render something, makes sense as we manage state, the reason we are using class based components and not functional component or dumb
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
             
